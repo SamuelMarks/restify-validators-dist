e4a4219d9ea2668e4cbdad42d4fcd21eabc4d3fd
Samuel Marks <samuelmarks@gmail.com>
2017-05-18 19:42:55 +1000

    Upgraded packages; TSLint addition conformance; typings.json=>package.json (for all but mine); new .d.ts syntax; moved from `function` to `=>`

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 "use strict";
-var tv4_1 = require('tv4');
-function has_body(req, res, next) {
+Object.defineProperty(exports, "__esModule", { value: true });
+var tv4_1 = require("tv4");
+exports.has_body = function (req, res, next) {
     if (!req.body)
         res.json(400, { error: 'ValidationError', error_message: 'Body required' });
     return next();
-}
-exports.has_body = has_body;
-function mk_valid_body_mw(json_schema, to_res) {
+};
+exports.mk_valid_body_mw = function (json_schema, to_res) {
     if (to_res === void 0) { to_res = true; }
-    return function valid_body(req, res, next) {
+    return function (req, res, next) {
         var body_is = tv4_1.validateMultiple(req.body, json_schema);
         if (!body_is.valid)
             (function (error) { return to_res ? res.json(400, error) : req['json_schema_error'] = error; })(body_is.errors.length === 1 ? {
@@ -27,7 +27,7 @@ function mk_valid_body_mw(json_schema, to_res) {
                             params: error.params,
                             schemaPath: error.schemaPath,
                             subErrors: error.subErrors
-                        }, process.env.DEBUG_LEVEL && parseInt(process.env.DEBUG_LEVEL) > 2 ? { stack: error.stack } : {});
+                        }, process.env['DEBUG_LEVEL'] && parseInt(process.env['DEBUG_LEVEL'], 10) > 2 ? { stack: error.stack } : {});
                     }),
                     missing: body_is['missing'],
                     valid: body_is['valid']
@@ -35,9 +35,8 @@ function mk_valid_body_mw(json_schema, to_res) {
             });
         return next();
     };
-}
-exports.mk_valid_body_mw = mk_valid_body_mw;
-function mk_valid_body_mw_ignore(json_schema, ignore) {
+};
+exports.mk_valid_body_mw_ignore = function (json_schema, ignore) {
     return function valid_body(req, res, next) {
         if (!req['json_schema_error'])
             return next();
@@ -47,7 +46,10 @@ function mk_valid_body_mw_ignore(json_schema, ignore) {
                     req['json_schema_error'].delete_me = true;
                     break;
                 case req['json_schema_error'].error_message === 'JSON-Schema validation failed':
-                    req['json_schema_error'].error_metadata.errors = req['json_schema_error'].error_metadata.errors.filter(function (error) { return error.message.substr(0, filter.length) !== filter; });
+                    req['json_schema_error'].error_metadata.errors =
+                        req['json_schema_error'].error_metadata.errors.filter(function (error) {
+                            return error.message.substr(0, filter.length) !== filter;
+                        });
                     break;
                 default:
                     console.warn('Unknown dataset received instead of json_schema_error');
@@ -59,12 +61,10 @@ function mk_valid_body_mw_ignore(json_schema, ignore) {
             delete req['json_schema_error'];
         return next();
     };
-}
-exports.mk_valid_body_mw_ignore = mk_valid_body_mw_ignore;
-function remove_from_body(keys) {
+};
+exports.remove_from_body = function (keys) {
     return function (req, res, next) {
         keys.map(function (key) { return req.body && req.body[key] ? delete req.body[key] : null; });
         return next();
     };
-}
-exports.remove_from_body = remove_from_body;
+};
